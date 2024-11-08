32fa2007fc30225953044b84af683da8925e1f39
dattumadhav <dattumadhav192d@gmail.com>
2024-11-08 10:56:12 +0530

    madhav2 file is commited

---
+++ madhav2.java
@@ -0,0 +1,9 @@
+public class madhav2 {
+    public static void main(String args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a=sc.nextInt();
+        System.out.println(n+a);
+    }
+    
+}
